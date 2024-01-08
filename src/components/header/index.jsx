034190f220eb856178339b762345a3b27be7a674
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-01-08 21:09:40 +0530

    base website done styling yet to be done

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,48 @@
 const  Header= () => {
   return (
-    <div>
-      Header
+    <div className="w-[1216px] h-16 bg-white justify-start items-start inline-flex">
+      <div className="w-[1216px] h-16 px-8 py-3 bg-white border-b border-zinc-300 justify-start items-center gap-4 flex">
+        <div className="grow shrink basis-0 h-7 justify-start items-center gap-6 flex">
+          <div className="flex-col justify-center items-start gap-0.5 inline-flex">
+            <div className="justify-start items-center gap-3 inline-flex">
+              <div className="justify-start items-center gap-2 flex">
+                <div className="justify-start items-center gap-4 flex">
+                  <div className="text-zinc-900 text-xl font-medium font-['Inter'] leading-7">Payments</div>
+                </div>
+                <div className="justify-start items-center gap-1.5 flex">
+                  <div className="w-3.5 h-3.5 relative" />
+                  <div className="text-neutral-600 text-xs font-normal font-['Inter'] leading-none">How it works</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-10 justify-start items-start flex">
+          <div className="grow shrink basis-0 h-10 px-4 py-2.5 bg-zinc-100 rounded border border-neutral-200 justify-start items-center gap-2 flex">
+            <div className="w-3.5 h-3.5 relative" />
+            <div className="grow shrink basis-0 text-zinc-500 text-sm font-normal font-['Inter'] leading-tight">Search features, tutorials, etc.</div>
+          </div>
+        </div>
+        <div className="grow shrink basis-0 h-10 justify-end items-center gap-3 flex">
+          <div className="justify-start items-center gap-3 flex">
+            <div className="justify-end items-center gap-1.5 flex">
+              <div className="w-4 h-4 relative" />
+              <div className="justify-end items-center gap-5 flex">
+                <div className="text-right text-neutral-600 text-[13px] font-medium font-['Inter'] leading-none">Need help?</div>
+                <div className="w-px h-6 bg-zinc-300" />
+              </div>
+            </div>
+            <div className="w-10 h-10 relative">
+              <div className="w-10 h-10 left-0 top-0 absolute bg-neutral-200 rounded-full" />
+              <div className="w-[18.89px] h-[22.22px] left-[10.56px] top-[8.89px] absolute">
+              </div>
+            </div>
+            <div className="w-10 h-10 relative">
+              <div className="w-10 h-10 left-0 top-0 absolute bg-neutral-200 rounded-full" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/main-page/index.jsx
+++ src/components/main-page/index.jsx
@@ -1,7 +1,125 @@
 const MainPage = () => {
   return (
-    <div>
-      MainPage
+    <div className="w-[760px] h-[712px] flex-col justify-start items-end gap-8 inline-flex">
+      <div className="flex-col justify-start items-end gap-8 flex">
+        <div className="flex-col justify-start items-start gap-6 flex">
+          <div className="p-6 bg-white rounded-lg shadow flex-col justify-start items-center gap-6 flex">
+            <div className="flex-col justify-start items-center gap-6 flex">
+              <div className="flex-col justify-start items-start gap-6 flex">
+                <div className="flex-col justify-start items-start gap-0.5 flex">
+                  <div className="w-[712px] text-zinc-900 text-base font-medium font-['Galano Grotesque'] leading-normal">Payment providers</div>
+                  <div className="text-zinc-500 text-sm font-normal font-['Galano Grotesque'] leading-tight">Set up payment providers to accept payments from your customers.</div>
+                </div>
+              </div>
+            </div>
+            <div className="flex-col justify-start items-start gap-4 flex">
+              <div className="w-[712px] rounded justify-start items-center gap-6 inline-flex">
+                <div className="grow shrink basis-0 h-16 justify-start items-center gap-4 flex">
+                  <div className="w-16 h-16 relative rounded border border-neutral-200">
+                    <div className="w-[48.77px] h-[30.14px] left-[8px] top-[17px] absolute">
+                      <div className="w-[48.77px] h-[9.49px] left-0 top-[20.65px] absolute">
+                      </div>
+                      <div className="w-[14.23px] h-4 left-[17.72px] top-0 absolute">
+                      </div>
+                    </div>
+                  </div>
+                  <div className="grow shrink basis-0 flex-col justify-start items-start gap-0.5 inline-flex">
+                    <div className="self-stretch text-zinc-900 text-base font-medium font-['Galano Grotesque'] leading-normal">Razorpay</div>
+                  </div>
+                </div>
+                <div className="px-4 py-2 rounded border border-sky-700 justify-center items-center gap-2.5 flex">
+                  <div className="text-center text-sky-700 text-sm font-medium font-['Galano Grotesque'] leading-tight">Set up</div>
+                </div>
+              </div>
+              <div className="w-[712px] rounded justify-start items-center gap-6 inline-flex">
+                <div className="grow shrink basis-0 h-16 justify-start items-center gap-4 flex">
+                  <div className="w-16 h-16 relative">
+                    <div className="w-16 h-16 left-0 top-0 absolute rounded border border-neutral-200" />
+                    <div className="w-[49px] h-[14.30px] left-[7.10px] top-[24.40px] absolute">
+                      <div className="w-[39.22px] h-[14.30px] left-[9.78px] top-[-0px] absolute">
+                      </div>
+                    </div>
+                  </div>
+                  <div className="grow shrink basis-0 flex-col justify-start items-start gap-0.5 inline-flex">
+                    <div className="self-stretch text-zinc-900 text-base font-medium font-['Galano Grotesque'] leading-normal">Cashfree Payments</div>
+                  </div>
+                </div>
+                <div className="px-4 py-2 rounded border border-sky-700 justify-center items-center gap-2.5 flex">
+                  <div className="text-center text-sky-700 text-sm font-medium font-['Galano Grotesque'] leading-tight">Set up</div>
+                </div>
+              </div>
+              <div className="w-[712px] rounded justify-start items-center gap-6 inline-flex">
+                <div className="grow shrink basis-0 h-16 justify-start items-center gap-4 flex">
+                  <div className="w-16 h-16 relative">
+                    <div className="w-16 h-16 left-0 top-0 absolute rounded border border-neutral-200" />
+                    <div className="w-[45.37px] h-[32.36px] left-[9px] top-[16px] absolute">
+                    </div>
+                  </div>
+                  <div className="grow shrink basis-0 flex-col justify-start items-start gap-0.5 inline-flex">
+                    <div className="self-stretch text-zinc-900 text-base font-medium font-['Galano Grotesque'] leading-normal">PhonePe Payment Gateway</div>
+                  </div>
+                </div>
+                <div className="px-4 py-2 rounded border border-sky-700 justify-center items-center gap-2.5 flex">
+                  <div className="text-center text-sky-700 text-sm font-medium font-['Galano Grotesque'] leading-tight">Set up</div>
+                </div>
+              </div>
+              <div className="w-[712px] rounded justify-start items-center gap-6 inline-flex">
+                <div className="grow shrink basis-0 h-16 justify-start items-center gap-4 flex">
+                  <div className="w-16 h-16 relative">
+                    <div className="w-16 h-16 left-0 top-0 absolute rounded border border-neutral-200" />
+                    <div className="w-12 h-[15px] left-[8px] top-[24px] absolute">
+                      <div className="w-12 h-[14.97px] left-[0.02px] top-[0.02px] absolute">
+                      </div>
+                    </div>
+                  </div>
+                  <div className="grow shrink basis-0 flex-col justify-start items-start gap-0.5 inline-flex">
+                    <div className="self-stretch text-zinc-900 text-base font-medium font-['Galano Grotesque'] leading-normal">Paytm Payment Gateway</div>
+                  </div>
+                </div>
+                <div className="px-4 py-2 rounded border border-sky-700 justify-center items-center gap-2.5 flex">
+                  <div className="text-center text-sky-700 text-sm font-medium font-['Galano Grotesque'] leading-tight">Set up</div>
+                </div>
+              </div>
+              <div className="w-[712px] rounded justify-start items-center gap-6 inline-flex">
+                <div className="grow shrink basis-0 h-16 justify-start items-center gap-4 flex">
+                  <div className="w-16 h-16 relative">
+                    <div className="w-16 h-16 left-0 top-0 absolute rounded border border-neutral-200" />
+                    <div className="w-10 h-4 left-[12px] top-[23px] absolute">
+                    </div>
+                  </div>
+                  <div className="grow shrink basis-0 flex-col justify-start items-start gap-0.5 inline-flex">
+                    <div className="self-stretch text-zinc-900 text-base font-medium font-['Galano Grotesque'] leading-normal">Cash on delivery</div>
+                    <div className="w-[576px] text-neutral-600 text-sm font-normal font-['Inter'] leading-tight">Receive payments in cash upon delivery.</div>
+                  </div>
+                </div>
+                <div className="w-9 h-[22px] relative">
+                  <div className="w-9 h-[22px] left-0 top-0 absolute">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="rounded-lg shadow flex-col justify-start items-center flex">
+            <div className="p-6 bg-white flex-col justify-start items-start gap-6 flex">
+              <div className="w-[712px] justify-start items-start gap-8 inline-flex">
+                <div className="grow shrink basis-0 flex-col justify-start items-start gap-0.5 inline-flex">
+                  <div className="self-stretch text-zinc-900 text-base font-medium font-['Galano Grotesque'] leading-normal">Manual payment methods</div>
+                  <div className="self-stretch text-zinc-500 text-sm font-normal font-['Inter'] leading-tight">Payments that are made outside online store. Allows you to accept cash, check or other custom forms of payment.</div>
+                </div>
+                <div className="w-9 h-[22px] relative">
+                  <div className="w-9 h-[22px] left-0 top-0 absolute">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-6 py-2 opacity-40 bg-sky-700 rounded justify-center items-center gap-2.5 inline-flex">
+        <div className="justify-start items-start gap-2 flex">
+          <div className="text-center text-white text-base font-medium font-['Inter'] leading-normal">Finish</div>
+        </div>
+      </div>
     </div>
   )
 }
