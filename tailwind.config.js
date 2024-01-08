7270c984e75dfbacb1bd15fa108ae8b7584f530a
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-01-08 19:41:49 +0530

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-100": "var(--black-100)",
+        "black-12": "var(--black-12)",
+        "black-30": "var(--black-30)",
+        "black-50": "var(--black-50)",
+        "black-60": "var(--black-60)",
+        "black-85": "var(--black-85)",
+        "black-90": "var(--black-90)",
+        "black-95": "var(--black-95)",
+        "black-98": "var(--black-98)",
+        "colorsblack-100": "var(--colorsblack-100)",
+        colorsprimaryblue: "var(--colorsprimaryblue)",
+        "primary-blue": "var(--primary-blue)",
+        "secondary-navbar": "var(--secondary-navbar)",
+        "secondary-red": "var(--secondary-red)",
+      },
+      fontFamily: {
+        "body-1-medium": "var(--body-1-medium-font-family)",
+        "body-2-medium": "var(--body-2-medium-font-family)",
+        "body-2-regular": "var(--body-2-regular-font-family)",
+        "body-default-medium": "var(--body-default-medium-font-family)",
+        "body-small-medium": "var(--body-small-medium-font-family)",
+        "body-small-regular": "var(--body-small-regular-font-family)",
+        "caption-medium": "var(--caption-medium-font-family)",
+        "caption-regular": "var(--caption-regular-font-family)",
+        "heading-h5": "var(--heading-h5-font-family)",
+        "sub-body-medium": "var(--sub-body-medium-font-family)",
+        "sub-body-regular": "var(--sub-body-regular-font-family)",
+        "sub-title-medium": "var(--sub-title-medium-font-family)",
+      },
+      boxShadow: {
+        "shadow-card": "var(--shadow-card)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
